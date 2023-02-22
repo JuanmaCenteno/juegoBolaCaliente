d1578a672b9545f274cdc24fa3fac4e9c407ae77
JuanmaCenteno <jmca111197@gmail.com>
2023-02-22 18:31:49 +0100

    Commitazo

---
+++ Juego/Assets/Scripts/movimientoJugador.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class movimientoJugador : MonoBehaviour
+{
+
+    public Camera camara;
+    public GameObject prefabSuelo;
+    public int velocidad;
+    private Vector3 offset;
+    private float valX, valZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = camara.transform.position;
+        valX = 0.0f;
+        valZ = 0.0f;
+        SueloInicial();
+    }
+
+    void SueloInicial(){
+        for(int i = 0; i < 3; i++){
+            valZ += 6.0f;
+            GameObject nuevoSuelo = Instantiate(prefabSuelo, new Vector3(valX, 0.0f, valZ), Quaternion.identity) as GameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
