7c93025689e1691f12ff4146b1eac4321aa1c37b
Javi <javiertw16@gmail.com>
2023-02-25 22:41:25 +0100

    Niveles terminados, adaptado suelo invisible para la caida y mucho más. Tengo sueño.

--- Juego/Assets/Scripts/movimientoJugador.cs
+++ Juego/Assets/Scripts/movimientoJugador.cs
@@ -2,38 +2,114 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Random = System.Random;
 
 public class movimientoJugador : MonoBehaviour
 {
-
+    Rigidbody rb;
     public Camera camara;
-    public GameObject prefabSuelo;
+    public GameObject Vacio;
     public int velocidad;
-    private Vector3 offset;
+    public int jumpspeed;
+    private Vector3 offset_camara;
+    private Vector3 offset_sombrero;
     private float valX, valZ;
 
+    private bool isGrounded = true;
+
+    private Random aleatorio;
+    private float tiempo;
+    private float ventanaTiempo;
+    private float tSiguienteSuelo;
+    public GameObject[] prefabsSuelos;
+    private int nSuelos;
+    private GameObject[] suelos;
+    private int indiceSuelos;
+
     public GameObject sombrero;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        offset = camara.transform.position;
-        offset = sombrero.transform.position;
-        valX = 0.0f;
+        rb = GetComponent<Rigidbody>();
+        offset_camara = camara.transform.position;
+        offset_sombrero = sombrero.transform.position;
         valZ = 0.0f;
+        valX = -12.0f;
+        aleatorio = new Random();
+        tiempo = 0;
+        ventanaTiempo = 6/velocidad;
+        tSiguienteSuelo = ventanaTiempo;
+        nSuelos = 5;
+        suelos = new GameObject[nSuelos];
+        indiceSuelos = 0;
         SueloInicial();
     }
 
     void SueloInicial(){
-        for(int i = 0; i < 3; i++){
-            valZ += 6.0f;
-            GameObject nuevoSuelo = Instantiate(prefabSuelo, new Vector3(valX, 0.0f, valZ), Quaternion.identity) as GameObject;
+        for(int i = 0; i<nSuelos; i++){
+            suelos[i] = Instantiate(prefabsSuelos[0], new Vector3(valX, 0.0f, valZ), Quaternion.identity) as GameObject;
+            valX += 6.0f;
         }
+    }
+
+    void crearSuelo(){
+        suelos[indiceSuelos] = Instantiate(prefabsSuelos[aleatorio.Next(prefabsSuelos.Length-1)], new Vector3(valX, 0.0f, valZ), Quaternion.identity) as GameObject;
+        indiceSuelos = (indiceSuelos+1)%nSuelos;
+        valX += 6.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        sombrero.transform.position = this.transform.position + offset;
+        //this.transform.position += new Vector3(0.0f, 0.0f,0.025f);
+        
+        float Hmove = Input.GetAxis("Horizontal");
+        float Vmove = Input.GetAxis("Vertical");
+        camara.transform.position = this.transform.position + offset_camara;
+        Vacio.transform.position = new Vector3(this.transform.position.x, -3.0f, this.transform.position.z);
+        sombrero.transform.position = this.transform.position + offset_sombrero;
+
+
+
+        Vector3 ballmove = new Vector3 (Hmove, 0.0f, Vmove);
+        rb.AddForce(ballmove * velocidad);
+
+        //SALTO
+        if(isGrounded && Input.GetKey(KeyCode.Space)){
+            Vector3 balljump = new Vector3(0.0f, 6.0f, 0.0f);
+            rb.AddForce(balljump * jumpspeed);
+        }
+
+        //GENERACION DE SUELOS POR TIEMPO
+        tiempo += Time.deltaTime;
+        if(tiempo > tSiguienteSuelo){
+            tSiguienteSuelo += ventanaTiempo;
+            suelos[indiceSuelos].SetActive(false);
+            crearSuelo();
+        }
+
+        //RESTART
+        if(Input.GetKeyDown(KeyCode.R)){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);     //Reiniciar si se pulsa la R, annadir reiniciar tiempo y puntuacion
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag ==  "Suelo"){
+            isGrounded = true;
+        }
+        if(other.gameObject.tag == "Vacio" || other.gameObject.tag == "Muerte"){
+            // DECIR QUE SE HA PERDIDO, MOMENTO GUITARTE
+        }
+    }
+
+    private void OnCollisionExit(Collision other) {
+        if(other.gameObject.tag ==  "Suelo"){
+            isGrounded = false;
+        }
     }
 }
